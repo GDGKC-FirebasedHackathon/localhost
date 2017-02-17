b74585d83fe54f08a6eae15e10e1ae17cbbe8328
jaeho <chojaeho1122@naver.com>
2017-02-17 19:47:19 +0900

    네비게이션 바 수정

--- app/src/main/java/com/example/lee/googlelogin/MainActivity.java
+++ app/src/main/java/com/example/lee/googlelogin/MainActivity.java
@@ -1,33 +1,29 @@
 package com.example.lee.googlelogin;
 
         import android.content.Intent;
-        import android.support.annotation.NonNull;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.TextView;
-        import android.widget.Toast;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-        import com.google.android.gms.auth.api.Auth;
-        import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-        import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-        import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-        import com.google.android.gms.common.ConnectionResult;
-        import com.google.android.gms.common.SignInButton;
-        import com.google.android.gms.common.api.GoogleApiClient;
-        import com.google.android.gms.common.api.ResultCallback;
-        import com.google.android.gms.common.api.Status;
-        import com.google.android.gms.tasks.OnCompleteListener;
-        import com.google.android.gms.tasks.Task;
-        import com.google.firebase.auth.AuthCredential;
-        import com.google.firebase.auth.AuthResult;
-        import com.google.firebase.auth.FirebaseAuth;
-        import com.google.firebase.auth.FirebaseUser;
-        import com.google.firebase.auth.GoogleAuthProvider;
-        import com.google.firebase.database.DatabaseReference;
-        import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
@@ -112,6 +108,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
             } else {
                 // Google Sign In failed, update UI appropriately
                 // ...
+
                 Toast.makeText(MainActivity.this, "NONONONONO", Toast.LENGTH_SHORT).show();
             }
         }
--- app/src/main/java/com/example/lee/googlelogin/TabMainActivity.java
+++ app/src/main/java/com/example/lee/googlelogin/TabMainActivity.java
@@ -2,9 +2,8 @@ package com.example.lee.googlelogin;
 
 import android.app.TabActivity;
 import android.graphics.drawable.Drawable;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
@@ -29,10 +28,11 @@ public class TabMainActivity extends TabActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tab_main);
         final DrawerLayout dl = (DrawerLayout)findViewById(R.id.drawerLayout);
         naviBtn = (ImageView) findViewById(R.id.naviBtn);
         final TabHost th = getTabHost();
+
         for(int i=0;i<tabsCount;i++){
             drawArray[i] = getResources().getDrawable(blackImage[i]);
 
